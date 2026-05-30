c897f2f720b07a74622947fff712fab0265f2f6b
Satyam Pandey <satyamxbuilds@gmail.com>
2026-05-30 13:08:07 +0530

    Implement score tracking for Snake, Water, Gun game
    Added scoring system to track user and computer scores.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 '''
 import random
 print("Welcome to Snake, Water, Gun game!")
+user_score = 0
+computer_score = 0
 computer = random.choice([1, -1, 0])
 youstr = input("Enter your choice: ")
 youDict = {"s": 1, "w": -1, "g": 0}
@@ -22,27 +24,41 @@ if(computer == you):
 else:
     if(computer == -1 and you == 1):
      print("You win!")
+        user_score +=1
      print("Computer chose water")
 
     elif(computer == -1 and you == 0):
       print("You lose!")
+        computer_score += 1
       print("Computer chose water")
 
     elif(computer == 1 and you == 0):
       print("You win!")
+        user_score +=1
+
       print("Computer chose snake")
 
     elif(computer == 1 and you == -1):
       print("You lose!")
+        computer_score += 1
+
       print("Computer chose snake")
 
     elif(computer == 0 and you == 1):
       print("You lose!")
+        computer_score += 1
+
       print("Computer chose Gun")
 
     elif(computer == 0 and you == -1):
       print("You win!")
+        user_score +=1
+
       print("Computer chose Gun")
 
     else:
       print("Something went wrong")
+
+print(f"\nScore:")
+print(f"You: {user_score}")
+print(f"Computer: {computer_score}")
